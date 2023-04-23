17fa6ef5b37ac914ee4e80600ef33163b83fe2a4
Ryan Moon <ryanmoondevelopment@gmail.com>
2023-04-23 17:38:31 -0500

    cool stuff

--- app/packages/backend/src/environment/environment.ts
+++
@@ -1,9 +0,0 @@
-import * as env from "env-var";
-
-export const environment = {
-  EMIT_GQL_SCHEMA: env.get("EMIT_GQL_SCHEMA").default("false").asBool(),
-  PORT: env.get("PORT").default(5555).asPortNumber(),
-  MONGOOSE_URL: env.get("MONGOOSE_URL").asUrlString(),
-  DB_NAME: env.get("MONGOOSE_DB_NAME").default("APP_DB").asString(),
-  JWT_PRIVATE_KEY: env.get("JWT_PRIVATE_KEY").default("sshh").asString(),
-};
--- app/packages/shared-utils/src/lib/environment/environment.config.ts
+++ app/packages/shared-utils/src/lib/environment/environment.config.ts
@@ -1,3 +1,5 @@
+import * as env from "env-var";
+
 import { backendConfig } from "./backend.config";
 import frontendConfig from "./frontend.config";
 
